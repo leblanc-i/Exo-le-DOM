1468e0e6f2bfe9087e930da52bfb4547d24ffc2f
idriss <idrissaamadouousmane@gmail.com>
2023-08-04 12:44:03 +0100

    Selecteurs et Click

--- cours/index.js
+++ cours/index.js
@@ -1,1 +1,17 @@
-// Selecteurs
+// ** Selecteurs **
+// document.querySelector("h4").style.background = "yellow"; 
+// const titre = document.querySelector("h4");
+
+// console.log(titre);
+
+// titre.style.background = "yellow";
+
+// *** Click event ***
+const questionBox = document.querySelector(".box");
+console.log(questionBox);
+
+// questionBox.style.borderRadius = "150px";
+
+questionBox.addEventListener("click", () => {
+    console.log("click !");
+})
